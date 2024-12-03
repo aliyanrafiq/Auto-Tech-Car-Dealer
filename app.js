83f0d2cf213e10b2ffc059c36ac41d6d7ee1154a
Muhammad Aliyan <120269166+aliyanrafiq@users.noreply.github.com>
2024-12-03 18:45:47 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var cars = {
+    honda: {
+        civic:{
+            type:{
+                name:'Honda Civic Reborn',
+                model:2024,
+                wheels:4,
+                gates: 4,
+                price: 8700000,
+                image: "https://cache4.pakwheels.com/system/car_generation_pictures/7370/original/Cover.jpg?1677570254" class="card-img-top" alt="..."
+            }
+        }
+    }
+}
+console.log(cars)
